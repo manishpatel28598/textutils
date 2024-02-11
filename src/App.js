55b1b1753793a37326b1885ecf3d5c8468912c6b
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-02-11 14:49:37 +0530

    adding dark mood enabling switch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import './App.css';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
+  const [mode, setmode] = useState('light')//change mode light or dark
+  const [alert, setAlert] = useState(null);
+  const showAlert=(message, type)=>{
+          setAlert({
+            msg: message,
+            type: type
+          })
+          setTimeout(()=>{
+             setAlert(null);
+          },1500)
+  }
+  const toggleMode = () =>{
+    if(mode==='light'){
+      setmode('dark');
+      document.body.style.backgroundColor ='#14103e';
+      document.body.style.color ='white';
+      showAlert(" Dark mode has been Enabled", "success");
+      
+    }
+    else{
+      setmode('light');
+      document.body.style.backgroundColor ='white';
+      document.body.style.color ='white';
+      showAlert(" Light mode has been Enabled", "success");
+    }
+  }
   return (
     <>
-<Navbar title = "Textutils" About = "About Textutils"/>
+<Navbar title = "Textutils" About = "About Textutils" mode={mode} toggleMode={toggleMode}/>
 {/* <Navbar/> */}
-<Textform heading = "Enter the test to analyze yourself "/>
+<Alert alert={alert}/>
+<Textform showAlert={showAlert} heading = "Enter the text to analyze the sentence" mode={mode} toggleMode={toggleMode}/>
   </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -5,23 +5,57 @@ const handleUpclick=()=>{
 
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert(" Converted to uppercase", "success");
+}
+const handleLoclick=()=>{
+
+  let newText = text.toLowerCase();
+  setText(newText);
+  props.showAlert(" Converted to Lowercase", "success");
+}
+const handleClearclick=()=>{
+
+  let newText ='';
+  setText(newText);
+  props.showAlert(" Cleared Text", "success");
 }
 const handleOnChange=(event)=>{
     setText(event.target.value);
 }
-
+const handleCopytxt=()=>{
+  let text = document.getElementById("my_box");
+  text.select();
+  navigator.clipboard.writeText(text.value);
+  props.showAlert(" Copied to Clipboard", "success");
+}
+const handleExtraSpaces=()=>{
+  let newtext = text.split(/[ ]+/);
+  setText(newtext.join(" "));
+  props.showAlert(" Extra Spaces removed", "success");
+}
     const [text, setText] = useState("Enter the text here");
   return (
-    <div>
-      <div className="container my-5">
-        <div className="mb-3 ">
+    <>
+    <div className="container">
           <h1>{props.heading}</h1>
-          <textarea className="form-control pt-1" id="my_box" value={text} onChange={handleOnChange} rows="7"></textarea>
-          <button className="btn btn-primary my-2" onClick={handleUpclick}>
-            Convert to uppercese
-          </button>
+        <div className="mb-3">
+          <textarea className="form-control pt-1" id="my_box" style={{backgroundColor: props.mode === 'dark'?'grey':'white', color:props.mode === 'dark'?'white':'black'}} value={text} onChange={handleOnChange} rows="7"></textarea>
         </div>
+          <button className="btn btn-primary my-2" onClick={handleUpclick}>Convert to uppercese</button>
+          <button className="btn btn-primary my-2 mx-2" onClick={handleLoclick}>Convert to Lowercese</button>
+          <button className="btn btn-primary my-2 " onClick={handleClearclick}>Clear Text</button>
+          <button className="btn btn-primary my-2 mx-2" onClick={handleCopytxt}>Copy Text</button>
+          <button className="btn btn-primary my-2 mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
       </div>
+    
+      <div className="container my-2">
+      <h1>Text Summary</h1>
+      <p>{text.split(" ").length} words and {(text.length+1)-(text.split(" ").length)} characters</p>
+      <p>{0.008*text.split(" ").length} minutes taken to read</p>
+      <h3>Preview</h3>
+      <p>{text.length>0?text:"Enter text in the above textbox to get peview here"}</p>
     </div>
+    
+    </>
   );
 }
